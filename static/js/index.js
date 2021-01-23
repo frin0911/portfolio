eaaab661ece97efc11acb17405d13abf66bf0a4e
UNKNOWN <44210830+frin0911@users.noreply.github.com>
2021-01-23 12:13:33 +0900

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,11 @@
 window.onload = function() {
     document.body.className += " loaded";
 }
+
+function checkBrowser() {
+    const agent = navigator.userAgent.toLowerCase();
+
+    if ((navigator.appName === 'Netscape' && navigator.userAgent.search('Trident') !== -1) || (agent.indexOf("msie") !== -1)) {
+        window.location.href = "ie.html";
+    }
+}
